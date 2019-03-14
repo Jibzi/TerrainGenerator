332ee0921d826afd15c9588e1358b2bf457e5b10
Jibzi <george.armstrong@mail.bcu.ac.uk>
2019-03-14 14:22:37 +0000

    Adds 1st script as test.

---
+++ TerrainGeneratorProject/Assets/Scripts/NoiseGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NoiseGenerator
+{
+    public static float[,] GenerateNoiseMap(int noiseMapWidth, int noiseMapHeight, float noiseScale)
+    {
+        return new float[noiseMapWidth, noiseMapHeight];
+    }
+}
